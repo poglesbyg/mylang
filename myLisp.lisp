db7e36594574e9948fcdf6da7ef54a38237f5daf
poglesby <poglesbyg@gmail.com>
2013-11-07 20:31:53 +0100

    first commit with eval worked on

---
+++ myLisp.lisp
@@ -0,0 +1,26 @@
+;;; mylisp is an implementation of McCarthy's Lisp
+;;; going to include numbers and strings
+;;; by Paul Grant
+
+(defun -eval (expr env)
+  (cond
+    ((atom expr) (cdr (assoc expr env)))
+    ((eq 'quote (car expr)) (-quote expr))
+    ((eq 'car (car expr)) (-car (-eval (cadr expr) env)))
+    ((eq 'cdr (car expr)) (-cdr (-eval (cadr expr) env)))
+    ((eq 'cons (car expr)) (-cons (-eval (cadr expr) env)
+				  (-eval (caddr expr) env)))))
+
+(defun -quote (expr)
+  (cadr expr))
+
+(defun -car (expr)
+  (car expr))
+
+(defun -cdr (expr)
+  (cdr expr))
+
+(defun -cons (expr1 expr2)
+  (cons expr1 expr2))
+
+
