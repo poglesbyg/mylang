ee618bba41ce08220b08e459560d828dd865f753
poglesby <poglesbyg@gmail.com>
2013-11-08 18:26:57 +0100

    starting to implement cond

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ mylang
 following McCarthy's 1960 essay on implementing lisp as well as some of Graham's essay on the "Roots of Lisp"
 
 TODO:
-- implement atom
-- implement eq
+- implement atom 
+- implement eq (done)
 - implement lambda
-- implement cond
+- implement cond: checks the car of the first argument returns TRUE then evaluate the next one otherwise go down to the next conditional
 
 THEN:
 - use mylang to implement the rest of lisp
--- myLisp.lisp
+++ myLisp.lisp
@@ -5,17 +5,18 @@
 (defun -eval (expr env)
   (cond
     ((atom expr) (cdr (assoc expr env)))
-    ((eq 'atom (car expr)) (-atom expr))
+    ((eq 'atom (car expr)) (-atom (-eval (cadr expr) env)))
     ((eq 'quote (car expr)) (-quote expr))
     ((eq 'car (car expr)) (-car (-eval (cadr expr) env)))
     ((eq 'cdr (car expr)) (-cdr (-eval (cadr expr) env)))
     ((eq 'cons (car expr)) (-cons (-eval (cadr expr) env)
 				  (-eval (caddr expr) env)))
     ((eq 'eq (car expr)) (-eq (-eval (cadr expr) env)
-			      (-eval (caddr expr) env)))))
+			      (-eval (caddr expr) env)))
+    ((eq 'cond (car expr)) (-cond (cadr expr) env))))
 
 (defun -atom (expr)
-  (atom (car expr)))
+  (atom expr))
 
 (defun -quote (expr)
   (cadr expr))
@@ -31,4 +32,8 @@
 
 (defun -eq (expr1 expr2)
   (eq expr1 expr2))
+
+(defun -cond (expr env)
+  (cond
+    ((-eval (caar expr) env) (-eval (cadr expr) env) )))
 
